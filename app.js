e04b57d4ec6ec9fc5f49c088615970b6cd30c0d1
jagadum <jarugumallinuthan@gmail.com>
2024-03-22 20:02:28 -0500

    gridadded

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var gridRouter = require('./routes/grid'); // Import the grid router
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/grid', gridRouter); // Add the grid router
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
