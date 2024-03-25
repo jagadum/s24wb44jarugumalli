be2d70a1d7db220ef82066fc6252cc5f0a96e0e1
jagadum <jarugumallinuthan@gmail.com>
2024-03-25 13:31:51 -0500

    added

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const dressRouter = require('./routes/dress'); // dress endpoint added
-const gridRouter = require('./routes/grid'); // grid endpoint added
-const pickRouter = require('./routes/pick'); // pick endpoint added
-
-const app = express();
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var dressRouter = require('./routes/dress'); //dress endpoint added
+var gridRouter = require('./routes/grid'); //grid endpoint added
+var pickRouter = require('./routes/pick');//pick endpoint added
+var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,11 +22,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter); 
-app.use('/dress', dressRouter); // dress endpoint added
-app.use('/grid', gridRouter); // grid endpoint added
-app.use('/pick', pickRouter); // pick endpoint added
-
+app.use('/users', usersRouter);
+app.use('/dress', dressRouter); //dress endpoint added
+app.use('/grid', gridRouter); //grid endpoint added
+app.use('/pick', pickRouter); //pick endpoint added
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
